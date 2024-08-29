516a1513c191e9adb42a03bd0162c60f87051729
Ostap Kushnir <kushnirostap04@gmail.com>
2024-08-29 16:45:54 +0300

    make build

---
+++ index.49277dfa.js
@@ -0,0 +1,2 @@
+var t=document.querySelector("div"),e=t.querySelector("img"),c=t.clientWidth,i=t.clientHeight,l=e.clientWidth,n=e.clientHeight;e.style.top="".concat((c-l)/2,"px"),e.style.left="".concat((i-n)/2,"px");
+//# sourceMappingURL=index.49277dfa.js.map
